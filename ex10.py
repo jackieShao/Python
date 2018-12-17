e92cdafdf9d89f928cba78e003f5fc9c363ec23a
jackieShao <43589386+jackieShao@users.noreply.github.com>
2018-12-17 01:13:25 -0500

    Add files via upload

--- ex10.py
+++ ex10.py
@@ -2,11 +2,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arrange(0.0, 2.0, 0.01)
-y = (np.sin(x-2)**2) * (np.exp(-x**2))
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('f(x)')
+# Data for plotting
+x = np.arange(0.0, 2.0, 0.01)
+y = (np.sin(x - 2) **2) * np.exp(-x **2)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+plt.xlabel('X')
+plt.ylabel('F(X)')
 plt.title('Exercise 10')
-plt.grid(True)
-plt.show()
